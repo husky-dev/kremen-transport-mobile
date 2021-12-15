106bad3a21e15a6ab1f4da51f2a3770459670e89
Jaroslav Khorishchenko <websnipter@gmail.com>
2021-12-15 19:09:30 +0200

    CI: build script added

--- src/core/config.ts
+++ src/core/config.ts
@@ -6,9 +6,9 @@ export type AppLogLevel = 'error' | 'warn' | 'info' | 'debug' | 'trace' | 'time'
 export const isAppLogLevel = (val: unknown): val is AppLogLevel =>
   isStr(val) && ['error', 'warn', 'info', 'debug', 'trace', 'time'].includes(val);
 
-export type AppEnv = 'prd' | 'dev';
+export type AppEnv = 'production' | 'development';
 
-export const isAppEnv = (val: unknown): val is AppEnv => isStr(val) && ['prd', 'dev'].includes(val);
+export const isAppEnv = (val: unknown): val is AppEnv => isStr(val) && ['production', 'development'].includes(val);
 
 interface AppConfig {
   env: AppEnv;
@@ -18,7 +18,7 @@ interface AppConfig {
 }
 
 export const config: AppConfig = {
-  env: isAppEnv(Config.APP_ENV) ? Config.APP_ENV : 'prd',
+  env: isAppEnv(Config.APP_ENV) ? Config.APP_ENV : 'production',
   log: {
     level: isAppLogLevel(Config.APP_LOG_LEVEL) ? Config.APP_LOG_LEVEL : 'trace',
   },
