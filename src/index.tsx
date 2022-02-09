683b3501ef111437eb8e52db42249ec76340294f
Jaroslav Khorishchenko <websnipter@gmail.com>
2022-02-09 11:15:02 +0200

    CodePush: update log added

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
+import { config } from '@core/config';
+import { Log } from '@core/log';
 import { StorageProvider } from '@core/storage';
 import MapScreen from '@screens/Map';
-import React from 'react';
-import { StyleSheet } from 'react-native';
 import * as Sentry from '@sentry/react-native';
-import { config } from '@core/config';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { extendTheme, NativeBaseProvider, theme } from 'native-base';
 import { colors } from '@styles';
+import { extendTheme, NativeBaseProvider } from 'native-base';
+import React, { PureComponent } from 'react';
+import { StyleSheet } from 'react-native';
+import codePush, { DownloadProgress } from 'react-native-code-push';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const log = Log('app');
 
 Sentry.init({
   dsn: config.sentry.dsn,
@@ -37,4 +41,29 @@ const styles = StyleSheet.create({
   screen: { flex: 1 },
 });
 
-export default Sentry.wrap(App);
+class AppWithCodePush extends PureComponent {
+  codePushStatusDidChange(status: codePush.SyncStatus) {
+    switch (status) {
+      case codePush.SyncStatus.CHECKING_FOR_UPDATE:
+        return log.debug('checking for updates');
+      case codePush.SyncStatus.DOWNLOADING_PACKAGE:
+        return log.info('downloading package');
+      case codePush.SyncStatus.INSTALLING_UPDATE:
+        return log.info('installing update');
+      case codePush.SyncStatus.UP_TO_DATE:
+        return log.debug('up-to-date');
+      case codePush.SyncStatus.UPDATE_INSTALLED:
+        return log.info('update installed');
+    }
+  }
+
+  codePushDownloadDidProgress(progress: DownloadProgress) {
+    log.debug('download progress', { total: progress.totalBytes, received: progress.receivedBytes });
+  }
+
+  render() {
+    return <App />;
+  }
+}
+
+export default Sentry.wrap(codePush(AppWithCodePush));
